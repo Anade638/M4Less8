0c916a45f178fcfed796c2041f5e0be109585ba4
Anade638 <136354049+Anade638@users.noreply.github.com>
2023-07-31 22:14:17 +0300

    Add files via upload

---
+++ WebAdv/app_advertisements/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class Advertisement(models.Model):
+    title = models.CharField("Заголовок", max_length=128)
+    description = models.TextField("Описание")
+    price = models.DecimalField("Цена", max_digits=10, decimal_places=2)
+    auction = models.BooleanField("Торг", help_text="Отметьте если торг уместен")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self) -> str:
+        return f"advertisements(id={self.id}, title={self.title}, price={self.price})"
+
+    class Meta:
+        db_table = "advertisements"
